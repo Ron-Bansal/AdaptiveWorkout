df931f32e42551ae0568bd46f70bf3c97a36d5a4
“Ron <raunaqbansal@hotmail.com>
2025-02-26 20:14:48 +1300

    Initial Commit

---
+++ AdaptiveWorkouts/AdaptiveWorkoutsApp.swift
@@ -0,0 +1,20 @@
+//
+//  AdaptiveWorkoutsApp.swift
+//  AdaptiveWorkouts
+//
+//  Created by Ron Bansal on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveWorkoutsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
